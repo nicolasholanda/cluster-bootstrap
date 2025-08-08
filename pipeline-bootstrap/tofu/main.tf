49261d5fe5ffdc2af6e28df31210f6e2a6acc257
Nicolas Holanda <nikolas.nhm14@gmail.com>
2025-08-08 08:30:06 -0300

    Added Jenkins initial config

--- pipeline-bootstrap/tofu/main.tf
+++ pipeline-bootstrap/tofu/main.tf
@@ -10,6 +10,78 @@ resource "kubernetes_namespace" "apps" {
   }
 }
 
+resource "kubernetes_service_account" "jenkins" {
+  metadata {
+    name      = "jenkins"
+    namespace = kubernetes_namespace.infra.metadata[0].name
+  }
+}
+
+resource "kubernetes_cluster_role" "jenkins" {
+  metadata {
+    name = "jenkins-robot"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods", "services", "endpoints", "persistentvolumeclaims", "configmaps", "secrets", "serviceaccounts"]
+    verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
+  }
+
+  rule {
+    api_groups = ["apps"]
+    resources  = ["deployments", "replicasets", "daemonsets", "statefulsets"]
+    verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
+  }
+
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources  = ["ingresses", "networkpolicies"]
+    verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
+  }
+
+  rule {
+    api_groups = ["rbac.authorization.k8s.io"]
+    resources  = ["roles", "rolebindings", "clusterroles", "clusterrolebindings"]
+    verbs      = ["get", "list", "watch", "create", "update", "patch", "delete"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["namespaces"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "jenkins" {
+  metadata {
+    name = "jenkins-robot"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.jenkins.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.jenkins.metadata[0].name
+    namespace = kubernetes_service_account.jenkins.metadata[0].namespace
+  }
+}
+
+resource "kubernetes_secret" "jenkins_kubeconfig" {
+  metadata {
+    name      = "jenkins-kubeconfig"
+    namespace = kubernetes_namespace.infra.metadata[0].name
+  }
+
+  data = {
+    "config" = file(var.kubeconfig_path)
+  }
+
+  type = "Opaque"
+}
+
 resource "kubernetes_deployment" "jenkins" {
   metadata {
     name = "jenkins"
@@ -32,6 +104,28 @@ resource "kubernetes_deployment" "jenkins" {
         }
       }
       spec {
+        service_account_name = kubernetes_service_account.jenkins.metadata[0].name
+        init_container {
+          name  = "install-tools"
+          image = "alpine:latest"
+          command = ["/bin/sh"]
+          args = ["-c", <<-EOT
+            apk add --no-cache curl && \
+            curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
+            install -o root -g root -m 0755 kubectl /shared/kubectl && \
+            curl -L -o tofu.tar.gz https://github.com/opentofu/opentofu/releases/download/v1.10.4/tofu_1.10.4_linux_amd64.tar.gz && \
+            tar -xzf tofu.tar.gz && \
+            mv tofu /shared/tofu && \
+            curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 && \
+            chmod 700 get_helm.sh && \
+            HELM_INSTALL_DIR=/shared ./get_helm.sh --no-sudo
+          EOT
+          ]
+          volume_mount {
+            name       = "shared-tools"
+            mount_path = "/shared"
+          }
+        }
         container {
           name  = "jenkins"
           image = "jenkins/jenkins:lts"
@@ -54,6 +148,24 @@ resource "kubernetes_deployment" "jenkins" {
           volume_mount {
             name       = "tmp"
             mount_path = "/tmp"
+          }
+          volume_mount {
+            name       = "kubeconfig"
+            mount_path = "/var/jenkins_home/.kube"
+            read_only  = true
+          }
+          volume_mount {
+            name       = "shared-tools"
+            mount_path = "/usr/local/bin"
+            read_only  = true
+          }
+          env {
+            name  = "KUBECONFIG"
+            value = "/var/jenkins_home/.kube/config"
+          }
+          env {
+            name  = "PATH"
+            value = "/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
           }
           liveness_probe {
             http_get {
@@ -90,6 +202,16 @@ resource "kubernetes_deployment" "jenkins" {
         }
         volume {
           name = "tmp"
+          empty_dir {}
+        }
+        volume {
+          name = "kubeconfig"
+          secret {
+            secret_name = kubernetes_secret.jenkins_kubeconfig.metadata[0].name
+          }
+        }
+        volume {
+          name = "shared-tools"
           empty_dir {}
         }
         security_context {
